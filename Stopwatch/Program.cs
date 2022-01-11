dc0fc0e4382ec25f3b4eb7a3c41da7dee3e8d085
Daniel Roberts <daniel.prescott.roberts@gmail.com>
2022-01-11 17:27:36 -0500

    Add project files.

---
+++ Stopwatch/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var watch = new Stopwatch();
+            watch.Start();
+            Console.ReadKey();
+            watch.Stop();
+            Console.WriteLine(watch.startTime);
+            Console.WriteLine(watch.stopTime);
+            Console.WriteLine(watch.duration);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Stopwatch/Stopwatch.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stopwatch
+{
+    class Stopwatch
+    {
+        public bool isRunning { get; set; }
+        public DateTime startTime { get; set; }
+        public DateTime stopTime { get; set; }
+        public TimeSpan duration { get; set; }
+
+        public void Start()
+        {
+            if (isRunning == true)
+            {
+                throw new Exception("Cannot start stopwatch. Already Running");
+            }
+            startTime = DateTime.Now;
+            isRunning = true;
+
+        }
+
+        public void Stop()
+        {
+            if(isRunning ==false)
+            {
+                throw new Exception("Stopwatch is not running.");
+            }
+
+            stopTime = DateTime.Now;
+            isRunning = false;
+
+            duration = stopTime - startTime;
+
+        }
+    }
+}
